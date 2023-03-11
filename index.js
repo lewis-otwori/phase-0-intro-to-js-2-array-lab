1965ae86156ede826458e5b7b7bd7284eaf2c8b3
lewis-otwori <lewisotwori@gmail.com>
2023-03-11 14:42:52 +0300

    Completed assingments

--- index.js
+++ index.js
@@ -1,1 +1,75 @@
-// Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+destructivelyAppendCat();
+
+console.log(cats);
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop(); 
+    return cats; 
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+  }
+  
+  
+
+  function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+  }
+  
+  
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+let newCats = prependCat('Garfield');
+
+console.log(cats); 
+console.log(newCats); 
+
+
+
+function removeLastCat() {
+  
+  const newCats = [...cats];
+  
+  
+  newCats.pop();
+
+  
+  return newCats;
+}
+
+function removeFirstCat() {
+  
+    const newCats = [...cats];
+    
+    
+    newCats.shift();
+  
+    
+    return newCats;
+  }
+
+console.log(cats); 
+console.log(removeLastCat());
+console.log(cats); 
+
+
+
+  
+  
